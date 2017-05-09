5033c472072b991237cba3536bba4374ccf651fe
Kuya Takami <kuya@gwu.edu>
2017-05-09 16:34:27 -0400

    vicon test

--- scripts/node.py
+++ scripts/node.py
@@ -5,6 +5,8 @@ import numpy as np
 from geometry_msgs.msg import PoseStamped
 from sensor_msgs.msg import Imu
 from controller import *
+import time
+
 
 class uav(object):
 
@@ -13,23 +15,29 @@ class uav(object):
         self.uav_pose = rospy.Subscriber('/vicon/Jetson/pose',PoseStamped, self.mocap_sub)
         self.uav_w = rospy.Subscriber('imu/imu',Imu, self.imu_sub)
         self.tf_subscriber = tf.TransformListener()
+        self.tf = tf.TransformerROS(True,rospy.Duration(10.0))
+
         self.x = np.zeros(3)
         self.v = np.zeros(3)
         self.R = np.eye(3)
-        self.W = np.eye(3)
-        e3 = np.array([0,0,1])
+        self.W = np.zeros(3)
+        b1 = np.array([1,0,0])
         self.x_c = (np.zeros(3), np.zeros(3),np.zeros(3),np.zeros(3),np.zeros(3),
-                e3, np.zeros(3), np.zeros(3),
+                b1, np.zeros(3), np.zeros(3),
                 np.eye(3),np.zeros(3),np.zeros(3))
-        self.controller = Controller(np.eye(3),np.ones(3))
+        J = np.diag([0.0820, 0.0845, 0.1377])
+        e3 = np.array([0.,0.,1.])
+        self.controller = Controller(J,e3)
+        self.F = None
+        self.M = None
         rospy.spin()
     def mocap_sub(self, msg):
         try:
             (trans,rot) = self.tf_subscriber.lookupTransform('/world', '/Jetson', rospy.Time(0))
             self.x = trans
-            # self.R = rot
+            self.R = self.tf.fromTranslationRotation(trans,rot)[:3,:3]
         except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
-            print('No transform between x and y found')
+            print('No transform between vicon and UAV found')
     def camera_sub(self):
         pass
     def imu_sub(self, msg):
@@ -37,8 +45,10 @@ class uav(object):
         self.W = np.array([w.x,w.y,w.z])
         self.control()
     def control(self):
-        # print(self.controller.position_control( self.R, self.W, self.x, self.v, self.x_c))
+        self.F, self.M = self.controller.position_control( self.R, self.W, self.x, self.v, self.x_c)
+    def motor_command(self):
         pass
+
 
 if __name__ == '__main__':
     print('starting tf_subscriber node')
