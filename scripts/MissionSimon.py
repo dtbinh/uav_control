b72417e188bf0445240d8f876b5a17a4d40e3127
kuya <kuya@gwu.edu>
2017-05-29 14:54:31 -0400

    take of and landing working

--- scripts/MissionSimon.py
+++ scripts/MissionSimon.py
@@ -1,13 +1,22 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 29 14:15:08 2017
+
+@author: simonlibine
+"""
+
 #!/usr/bin/env python
 import time
 import rospy
 from uav_control.msg import trajectory
 import pygame
 import sys
+from trajectory_tracking_FOR_HADWARE import desired_pos, initialisation
 pygame.init()
 pygame.display.set_mode((300,300))
 
-mission =  {'mode':'init','motor':False,'warmup':False}
+mission =  {'mode':'init'}
 z_min = 0.15
 z_hover = 1.5
 v_up = 0.5
@@ -17,12 +26,12 @@ def get_key():
         if event.type == pygame.QUIT:
             sys.exit()
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_f:
+            if event.key == pygame.K_w:
                 mission['Forward'] = True
                 print('Forward')
             elif event.key == pygame.K_s:
-                mission['Backward'] = True
-                print('Backward')
+                mission['Simon'] = True
+                print('Simon mission')
             elif event.key == pygame.K_q:
                 sys.exit()
             elif event.key == pygame.K_t:
@@ -34,12 +43,6 @@ def get_key():
             elif event.key == pygame.K_h:
                 mission['mode'] = 'hover'
                 print('Hovering at origin')
-            elif event.key == pygame.K_m:
-                mission['motor'] = not mission['motor']
-                print('Motor on')
-            elif event.key == pygame.K_w:
-                mission['warmup'] = not mission['warmup']
-                print('Motor warmup')
 
 #while True:
 #    get_key()
@@ -55,32 +58,7 @@ def mission_request():
     cmd = trajectory()
     cmd.b1 = [1,0,0]
     cmd.header.frame_id = 'uav'
-
-    if mission['motor'] == True:
-        if rospy.get_param('/odroid_node/Motor'):
-            rospy.set_param('/odroid_node/Motor', False)
-            print('Motor OFF')
-        else:
-            rospy.set_param('/odroid_node/Motor', True)
-            print('Motor ON')
-        rospy.set_param('/odroid_node/MotorWarmup', True)
-        pub.publish(cmd)
-        mission['motor'] = False
-    elif mission['warmup'] == True:
-        if rospy.get_param('/odroid_node/MotorWarmup'):
-            rospy.set_param('/odroid_node/MotorWarmup', False)
-            print('Motor warmup OFF')
-        else:
-            rospy.set_param('/odroid_node/MotorWarmup', True)
-            print('Motor warmup ON')
-        pub.publish(cmd)
-        mission['warmup'] = False
     if mission['mode'] == 'takeoff':
-        rospy.set_param('/odroid_node/Motor', True)
-        rospy.set_param('/odroid_node/MotorWarmup', True)
-        print('Motor warmup ON')
-        rospy.sleep(1)
-        rospy.set_param('/odroid_node/MotorWarmup', False)
         print('Taking off at {} sec'.format(time.time()-t_init))
         t_total = 5
         t_cur= 0
@@ -97,7 +75,7 @@ def mission_request():
         print('Take off complete')
     elif mission['mode'] == 'land':
         print('Landing')
-        t_total = 4
+        t_total = 5
         t_cur = 0
         while t_cur <= t_total:
             t_cur = time.time() - t_init
@@ -108,12 +86,30 @@ def mission_request():
             cmd.xc_dot = [0,0,-v_up]
             pub.publish(cmd)
             get_key()
-        rospy.set_param('/odroid_node/Motor', False)
         mission['mode'] = 'wait'
         print('landing complete')
     elif mission['mode'] == 'hover':
         mission['mode'] = 'wait'
         pass
+    elif mission['mode'] == 'Simon':
+        print('Simon')
+        t_total = 140
+        t_cur = 0
+        x0 = x_v #[0,0,0]
+        dictionnary = initialisation(x0[0],x0[1],x0[2])
+        while t_cur <= t_total:
+            t_cur = time.time() - t_init
+            time.sleep(dt)
+            cmd.header.stamp = rospy.get_rostime()
+            height = z_hover - v_up*t_cur
+            desired_pos = desired_pos(t_cur,x_v,dictionnary)
+            cmd.xc = desired_pos[0]
+            cmd.xc_dot = desired_pos[1]
+            cmd.xc_2dot = desired_pos[2]
+            pub.publish(cmd)
+            get_key()
+        mission['mode'] = 'wait'
+        print('Simon mission complete')
     else:
         pass
         #print('command not found: try again')
